8026c2e5d8ebe9656c9b08db3a806282225dbf95
net-ari <originz11@proton.me>
2026-05-11 15:56:16 +0100

    Started UI functionality

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
+# imports
+    # ui?
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+    # my classes
 from die import Die
 from roller import Roller
 
-r: Roller = Roller()
+die1: Die = Die(6)
+die2: Die = Die(20)
 
-for i in range(5,21):
-    r.add(Die(i))
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-results = r.rollAll()
+        self.setWindowTitle("My first PyQt App!")
+        self.setFixedSize(400,300)
+        self.button_checked = True
 
-for result in results:
-    print(f"{result[0]} : {result[1]}")
+        button = QPushButton("Push me!")
+        button.setCheckable(True)
+        button.clicked.connect(self.button_clicked)
+        button.clicked.connect(self.button_toggled)
+        button.setChecked(self.button_checked)
+
+        self.setCentralWidget(button)
+
+    def button_clicked(self):
+        print("Clicked")
+    
+    def button_toggled(self, checked):
+        self.button_checked = checked
+        print(self.button_checked)
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()

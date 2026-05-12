24857f3bd10e3ccf33bd61c9260d64fd21e060d6
net-ari <originz11@proton.me>
2026-05-12 17:20:43 +0100

    Basic GUI Prototype implemented

--- die.py
+++ die.py
@@ -7,6 +7,9 @@ class Die:
         sides (int): The number of sides of the die.
     """
     def __init__(self,dSides):
+        if not(isinstance(dSides, int)) or dSides < 2 or dSides > 9999:
+            raise ValueError(f"sides must be an integer >= 2 and < 9999, got {dSides}")
+
         self.sides = dSides
         self.name = f"d{dSides}"
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # imports
     # ui?
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QLineEdit, QVBoxLayout, QWidget
     # my classes
 from die import Die
 from roller import Roller
@@ -12,24 +12,41 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My first PyQt App!")
-        self.setFixedSize(400,300)
-        self.button_checked = True
+        self.roller = Roller()
 
-        button = QPushButton("Push me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.button_clicked)
-        button.clicked.connect(self.button_toggled)
-        button.setChecked(self.button_checked)
+        self.setWindowTitle("Dice Roller")
 
-        self.setCentralWidget(button)
+        self.input: QLineEdit = QLineEdit()
+        self.create_button: QPushButton = QPushButton("Create Die")
+        self.label: QLabel = QLabel()
+        self.roll_button: QPushButton = QPushButton("Roll Die")
 
-    def button_clicked(self):
-        print("Clicked")
+        self.create_button.clicked.connect(self.create_die)
+        self.roll_button.clicked.connect(self.roll_die)
+
+        layout: QVBoxLayout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.create_button)
+        layout.addWidget(self.label)
+        layout.addWidget(self.roll_button)
+        
+        container: QWidget = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
     
-    def button_toggled(self, checked):
-        self.button_checked = checked
-        print(self.button_checked)
+    def create_die(self) -> None:
+        text = int(self.input.text())
+
+        die: Die = Die(text)
+        self.roller.add(die)
+
+        self.label.setText(die.get_name())
+
+    def roll_die(self) -> None:
+        results = self.roller.rollAll()
+
+        self.label.setText(f"Dice: {results[0][0]}, Result: {results[0][1]}")
 
 app = QApplication([])
 window = MainWindow()

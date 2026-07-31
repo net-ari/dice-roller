7e05c49cb4bd9d4cdfbe8af59ab35bf7718af6a7
net-ari <originz11@proton.me>
2026-07-31 16:00:57 +0100

    Began work on UI. For real this time I swear

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 from core import Die, Roller
+import sys
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__() 
+        self.setWindowTitle("Dice Roller")
+
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+        self.button.setEnabled(False)
+
+app = QApplication([])
+
+window = MainWindow()
+window.show()
+
+app.exec()

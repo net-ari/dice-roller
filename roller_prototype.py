d97d97cb5d9e4726c7c7ea3a9ca15d19d3b7d2ac
net-ari <originz11@proton.me>
2026-08-30 17:12:15 +0100

    Added functionality to Confirm button

--- roller_prototype.py
+++ roller_prototype.py
@@ -12,28 +12,32 @@ class MainWindow(QMainWindow):
         widget = QWidget()
         main_layout = QGridLayout()
 
+        # control row
+        self.reset_button = QPushButton("Reset")
+        self.reset_button.setEnabled(False)
+        self.roll_button = QPushButton("Roll")
+        self.roll_button.setEnabled(False)
+        self.confirm_button = QPushButton("Confirm")
+        self.confirm_button.clicked.connect(self.confirm_selection)
         controls = QHBoxLayout()
+        controls.addWidget(self.reset_button)
+        controls.addWidget(self.roll_button)
+        controls.addWidget(self.confirm_button)
 
-        controls.addWidget(QPushButton("Reset"))
-        controls.addWidget(QPushButton("Roll"))
-        controls.addWidget(QPushButton("Confirm"))
-
+        # dice pool (where selected dice should show up)
         pool_scroll = QScrollArea()
         pool_scroll.setWidgetResizable(True)
-
         pool_content = QWidget()
-        pool_layout = QGridLayout(pool_content)
-
-        pool_layout.addWidget(QLabel("Dice Pool"), 0, 0, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.pool_layout = QGridLayout(pool_content)
+        # pool_layout.addWidget(QLabel("Dice Pool"), 0, 0, alignment=Qt.AlignmentFlag.AlignCenter)
         pool_scroll.setWidget(pool_content)
 
+        # results of dice rolls
         results_scroll = QScrollArea()
         results_scroll.setWidgetResizable(True)
-
         results_content = QWidget()
-        results_layout = QGridLayout(results_content)
-
-        results_layout.addWidget(QLabel("Results"), 0,0, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.results_layout = QGridLayout(results_content)
+        # results_layout.addWidget(QLabel("Results"), 0,0, alignment=Qt.AlignmentFlag.AlignCenter)
         results_scroll.setWidget(results_content)
 
         # dice input button generation code 
@@ -44,9 +48,6 @@ class MainWindow(QMainWindow):
             button.clicked.connect(lambda _,d=die_type: self.roller.add(Die(d)))
             main_layout.addWidget(button,row,0)
 
-        # main_layout.addWidget(QPushButton("Reset"),6,1)
-        # main_layout.addWidget(QPushButton("Roll"),6,2)
-        # main_layout.addWidget(QPushButton("Confirm"),6,3)
         main_layout.addLayout(controls,6,1,1,3)
         main_layout.addWidget(QLabel("Total:"),6,4)
         main_layout.addWidget(pool_scroll,0,1,6,3)
@@ -54,13 +55,24 @@ class MainWindow(QMainWindow):
 
         main_layout.setColumnStretch(0,0)
         main_layout.setColumnStretch(1,3)
-        main_layout.setColumnStretch(4,1)  
-
-        # widget.setContentsMargins(0,0,0,0)
-        # main_layout.setContentsMargins(0,0,0,0)
+        main_layout.setColumnStretch(4,1) 
 
         widget.setLayout(main_layout)
         self.setCentralWidget(widget)
+
+    def confirm_selection(self):
+        NUM_COLUMNS = 3
+        for idx, die in enumerate(self.roller.getAll()):
+            die_label = QLabel(f"{die.get_name()}")
+            die_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+            die_label.setMinimumHeight(50)
+            die_label.setMinimumWidth(50)
+            row = idx // NUM_COLUMNS
+            col = idx % NUM_COLUMNS
+            self.pool_layout.addWidget(die_label,row,col)
+        self.confirm_button.setEnabled(False)
+        self.reset_button.setEnabled(True)
+        self.roll_button.setEnabled(True)
 
 app = QApplication([])
 window = MainWindow()
